a443c70e588c05c1aa488ccb5db3571c760e9069
Thanh Nguyen <thanhnguyen@bbv.ch>
2019-08-12 18:42:24 +0700

    Add create/edit restaurant

--- src/app/+admin/restaurant/restaurant.module.ts
+++ src/app/+admin/restaurant/restaurant.module.ts
@@ -3,9 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RestaurantComponent } from './restaurant.component';
 import { RestaurantRoutingModule } from './restaurant-routing.module';
 import { SimpleMaterialModule } from 'src/app/shared/modules/simple-material.module';
+import { RestaurantItemComponent } from './restaurant-item/restaurant-item.component';
+import { MatFormFieldModule, MatInputModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, RestaurantRoutingModule, SimpleMaterialModule],
-  declarations: [RestaurantComponent]
+  imports: [
+    FormsModule,
+    CommonModule,
+    RestaurantRoutingModule,
+    SimpleMaterialModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  declarations: [RestaurantComponent, RestaurantItemComponent],
+  entryComponents: [RestaurantItemComponent]
 })
 export class RestaurantModule {}
