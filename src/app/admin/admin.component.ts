f5cf521e6eafd12902f17a37337f5a3a7dc6c6c9
BBV\toannguyen <toan.nguyen@bbv.ch>
2019-09-24 13:55:28 +0700

    implement sample Notification

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -18,4 +18,32 @@ export class AdminComponent implements OnInit {
   onMenu() {
     this.router.navigate(['admin', 'menu']);
   }
+
+  testNotification() {
+    this.notifyMe();
+  }
+  notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!('Notification' in window)) {
+      alert('This browser does not support system notifications');
+      // This is not how you would really do things if they aren't supported. :)
+    }
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === 'granted') {
+      // If it's okay let's create a notification
+      const notification = new Notification('Hi there!');
+    }
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== 'denied') {
+      Notification.requestPermission(permission => {
+        // If the user accepts, let's create a notification
+        if (permission === 'granted') {
+          const notification = new Notification('Hi there!');
+        }
+      });
+    }
+
+    // Finally, if the user has denied notifications and you
+    // want to be respectful there is no need to bother them any more.
+  }
 }
